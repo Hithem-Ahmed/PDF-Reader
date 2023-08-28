df0ac706dfe9d70845601ca5b432e36ac4b39cf3
hahmed <hitham.ahmed@cegedim.com>
2023-08-28 18:14:04 +0300

    adjust the readPDFFromURL parameter

--- src/main/java/com/pdf/PDFReaderTest.java
+++ src/main/java/com/pdf/PDFReaderTest.java
@@ -1,12 +1,10 @@
 package com.pdf;
 
-import com.itextpdf.text.Document;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
 import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
 
-import java.io.ByteArrayInputStream;
 import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,13 +12,13 @@ import java.net.http.HttpResponse;
 
 public class PDFReaderTest {
 
-    private static String URL = "https://www.africau.edu/images/default/sample.pdf";
-    private static int PAGE_NUMBER = 2;
-
     public static void main(String[] args) {
 
+        final String URL = "https://www.africau.edu/images/default/sample.pdf";
+        final int PAGE_NUMBER = 2;
+
         try {
-            byte[] bytes  = readPDFFromURL();
+            byte[] bytes  = readPDFFromURL(URL);
             PdfReader reader = new PdfReader(bytes);
             TextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
             String currentText = PdfTextExtractor.getTextFromPage(reader, PAGE_NUMBER, strategy);
@@ -31,18 +29,16 @@ public class PDFReaderTest {
 
     }
 
-    public static byte[] readPDFFromURL() throws Exception {
+    public static byte[] readPDFFromURL(String url) throws Exception {
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri( new URI(URL))
+                .uri( new URI(url))
                 .version(HttpClient.Version.HTTP_2)
                 .GET()
                 .build();
 
         HttpClient client = HttpClient.newBuilder().build();
-
         HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
-
         return response.body();
     }
 }
